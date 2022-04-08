b650fb7c6b224a20fa8da55af1fcced02b938f40
P.angelino <p.angelinodaniele@gmail.com>
2022-04-08 16:43:14 +0200

    Add project files.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        
+        private int codice;
+
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+
+
+
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+            
+            
+
+        }
+
+
+        //--------------GETTERS AND SETTERS------------
+        public long getCodice()
+        {
+            
+
+            return codice;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+        public string getDescrizione()
+        {
+            return descrizione;
+        }
+        public double getPrezzo()
+        {
+            return prezzo;
+        }
+        public double getIva()
+        {
+            return iva;
+        }
+
+       
+
+        public void setPrezzo(double nuovoPrezzo)
+        {
+            if (nuovoPrezzo < 0)
+            {
+                return;
+            }else
+            {
+                prezzo = nuovoPrezzo;   
+            }
+        }
+
+        public void setIva(double nuovaIva)
+        {
+            if (nuovaIva < 0)
+            {
+                return;
+            }
+            else
+            {
+               iva = nuovaIva;
+            }
+        }
+        public void setDescrizione(string nuovaDescrizione)
+        {
+            descrizione = nuovaDescrizione; 
+        }
+
+        public void setNome(string nuovoNome)
+        {
+            nome = nuovoNome;
+        }
+
+        //------------------------------------------------------
+
+
+
+        //----------------------FUNZIONI------------------------
+
+        public void nomeEsteso()
+        {
+            Console.WriteLine("Il codice + nome è: " + codice + "-" + nome);
+        }
+
+
+        public void padLeft()
+        {
+            string codiceString = codice.ToString();
+            char zero = '0';
+            Console.WriteLine("Il codice per intero è: " + codiceString.PadLeft(8, zero));
+        }
+
+
+
+
+
+
+        public int ottieniCodice()
+        {
+            Random random = new Random();
+            codice = random.Next(1000);
+            return codice;
+        }
+
+
+
+        public double prezzoConIva()
+        {
+            double totale;
+            iva = (100 - iva) / 100;
+            totale = prezzo * iva;
+            Console.WriteLine("Il prezzo con iva di " + nome + " è: " + totale + " euro.");
+            return totale;
+
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,14 @@
+﻿using csharp_oop_shop;
+
+
+Prodotto uova = new Prodotto("uova", "uovo", 2, 20);
+
+uova.ottieniCodice();
+double prezzo = uova.getPrezzo();
+Console.WriteLine("Il prezzo è: " + prezzo);  
+uova.prezzoConIva();
+
+uova.nomeEsteso();
+uova.padLeft();
+
+
